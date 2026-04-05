8d2296c3b8d71ebae1fb6d66889d28acd9c57793
Marshall Warners <m@marshallwarners.com>
2026-04-04 21:06:30 -0400

    Consolidate to pyproject.toml

--- meeshbot/shell.py
+++ meeshbot/shell.py
@@ -5,5 +5,6 @@ Run with: uv run --env-file .env ipython -i meeshbot/shell.py
 
 from meeshbot.config import TESTING_GROUP_ID
 from meeshbot.integrations.groupme.client import GroupMeClient
+from meeshbot.models import GroupMeBot, GroupMeGroup, GroupMeMessage, GroupMeUser
 
 client = GroupMeClient()
