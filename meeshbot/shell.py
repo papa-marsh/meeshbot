cb4d6adc4b60f62eadf7493292064da6d71bf8bf
Marshall Warners <m@marshallwarners.com>
2026-04-04 21:28:33 -0400

    Shell improvements

--- meeshbot/shell.py
+++ meeshbot/shell.py
@@ -1,10 +1,16 @@
 """
 Interactive shell with pre-loaded meeshbot context.
-Run with: uv run --env-file .env ipython -i meeshbot/shell.py
+Run with: make shell
 """
 
-from meeshbot.config import TESTING_GROUP_ID
+import asyncio
+
+from oxyde import db
+
+from meeshbot.config import DATABASE_URL, TESTING_GROUP_ID
 from meeshbot.integrations.groupme.client import GroupMeClient
 from meeshbot.models import GroupMeBot, GroupMeGroup, GroupMeMessage, GroupMeUser
+
+asyncio.run(db.init(default=DATABASE_URL))
 
 client = GroupMeClient()
